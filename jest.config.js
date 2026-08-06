a265a1bb28d45ee92c2bf2ee71ce160d3e52956e
bakkunbb <bakkunbb@gmail.com>
2026-08-06 18:14:07 +0900

    chore: Jest + ts-jest 테스트 환경 설정

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  preset: '@react-native/jest-preset',
+  // preset: '@react-native/jest-preset',
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/test'],
+  moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' },
 };
