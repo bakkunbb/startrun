0c8fcef7f81de6b89dfe156d09c0a0281097f2b2
bakkunbb <bakkunbb@gmail.com>
2026-08-18 16:50:27 +0900

    chore: babel module-resolver 설정 수정

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
-    ['module-resolver', { root: ['./'], alias: { '@': './src' } }],
+    ['module-resolver', {
+      root: ['./src'],
+      alias: { '@': './src' },
+      extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+    }],
   ],
 };
